1b1886902c4274f00bb1e9d22b7dba9c58a160ed
nasser.gonzalez@gmail.com <nasser.gonzalez@harness.io>
2023-03-20 17:28:34 +0000

    Terraform output tutorial

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 # Output declarations
+output "vpc_id" {
+  description = "ID of project VPC"
+  value       = module.vpc.vpc_id
+sensitive   = true
+}
+
+output "web_server_count" {
+  description = "Number of web servers provisioned"
+  value       = length(module.ec2_instances.instance_ids)
+}
+
